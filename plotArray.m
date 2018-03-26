b5a5f26585cf2f87d05f79cf4cc0ca155dca40b1
Santi Rodrigo <santirodrigom@gmail.com>
2018-03-26 17:50:32 +0200

    Removing old files

--- plotArray.m
+++
@@ -1,8 +0,0 @@
-function plotArray(patch,Color,MarkerSize)
-    scatter3(patch(1,:),patch(2,:),patch(3,:),MarkerSize,'Marker','s','MarkerFaceColor',Color,'MarkerEdgeColor',Color);
-    xlabel('x','FontSize',14);
-    ylabel('y','FontSize',14);
-    zlabel('z','FontSize',14);
-    title('Array Patch','FontSize',15);
-    set(gca,'View',[0 90]);
-end
--- position_Constraint.m
+++
@@ -1,8 +0,0 @@
-function val = position_Constraint(X,problem)
-%POSITION_CONSTRAINT Non-linear constraint for Antenna Subarray matching
-%   	The positions must be integer values
-    act_pos = X(1:problem.Nmax);
-    val = sum(act_pos == floor(act_pos)) < problem.Nmax;
-    % Double values found if sum < N_MaxAnt (val == 1)
-end
-
