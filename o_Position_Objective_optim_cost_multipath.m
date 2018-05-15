15bac8ad68db546e1535f6bc6e4bf29c074a0179
root <bocanegra.c@husky.neu.edu>
2018-05-15 17:26:45 -0500

    Modified Objective function. Polished minor issues.

--- o_Position_Objective_optim_cost_multipath.m
+++ o_Position_Objective_optim_cost_multipath.m
@@ -1,6 +1,11 @@
 function [Val] = o_Position_Objective_optim_cost_multipath(Position_Taper, conf, problem)
-% Copyright 2017  The MathWorks, Inc.
-% Define constants
+    
+    % Initialize optimization variables
+    PR   = 0;  % Received power at intended user
+    Int  = 0;  % Interference generated to other users
+    HPBW = 0;  % Beamdiwth achieved
+    
+    % Define constants
     if ~strcmp(conf.genStructure, 'allAntennas')
         nck = strcmp(conf.genStructure, 'nchoosek');
         if sum(Position_Taper(1:problem.Nmax-(problem.Nmax*nck)+1) ~= ...
@@ -11,59 +16,48 @@ function [Val] = o_Position_Objective_optim_cost_multipath(Position_Taper, conf,
         end
     end
 
-    % Setting figures to invisible
-    %set(groot,'defaultFigureVisible','off')
-
-    azimuth = -180:2:180;
-    elevation = -90:2:90;
-
+    % Create antenna subarray
     [Position_Taper_elem,problem] = o_subarrayToAntennaElements(Position_Taper,conf,problem);
     % Extracting taper values from the input vector
     Taper_value = Position_Taper_elem(problem.ant_elem+1:problem.ant_elem*2) .* ...
         exp(1i.*Position_Taper_elem(problem.ant_elem*2+1:problem.ant_elem*3));
-
     if isempty(Taper_value)
-        disp('uyuyuy');
+        fprintf('warning: Taper is empty.\n');
     end
-    
+    % Creating a Conformal Array with cosine elements.
+    % Conformal array will be limited to a single plane
     handle_Conf_Array = phased.ConformalArray('Element',problem.handle_Ant,...
-        'ElementPosition',[zeros(1,problem.ant_elem);...
-        problem.possible_locations(2,Position_Taper_elem(1:problem.ant_elem));...
-        problem.possible_locations(3,Position_Taper_elem(1:problem.ant_elem))],...
-        'Taper',Taper_value);
+                        'ElementPosition',[zeros(1,problem.ant_elem);...
+                        problem.possible_locations(2,Position_Taper_elem(1:problem.ant_elem));...
+                        problem.possible_locations(3,Position_Taper_elem(1:problem.ant_elem))],...
+                        'Taper',Taper_value);
     
-    if conf.Fweights(2) > 0
-        bw_az = o_beamwidth(handle_Conf_Array,problem.freq,...
-            azimuth,problem.thetaChannels(problem.IDUserAssigned),3,...
-            conf.PlotAssignments);
-        if conf.verbosity > 1
-            fprintf('The HPBW (az) computed by the helper function is %f\n',bw_az);
-        end
-        bw_el = o_beamwidth(handle_Conf_Array,problem.freq,...
-            problem.phiChannels(problem.IDUserAssigned),elevation,3,...
-            conf.PlotAssignments);
-        if conf.verbosity > 1
-            fprintf('The HPBW (el) computed by the helper function is %f\n',bw_el);
-        end
-
-        HPBW = bw_az * bw_el;
-    end
-    
-    if conf.Fweights(1) > 0
+    % Consider rx power and interference generated into opt. (IUI metric)
+    if conf.Fweights(1)+conf.Fweights(2) > 0
         % Computing the received power in every user
         PRx = zeros(1,problem.nUsers);
         for u1=1:problem.nUsers
             for i=1:problem.maxnChannelPaths
                 if problem.phiChannels(u1,i) ~= -Inf
+                    % We assume that alphaChannels include the increase in 
+                    % distance from dUsers(u1) to the real distance traver-
+                    % sed by the signal in on each path
+                    
+                    % Pathloss into consideration (old)
                     PRx(u1) = PRx(u1) + problem.alphaChannels(u1,i)*...
-                        pattern(handle_Conf_Array,problem.freq,...
-                        problem.phiChannels(u1,i),...
-                        problem.thetaChannels(u1,i),...
-                        'Type','Power','Normalize',false)*...
-                        (problem.lambda/(4*pi*problem.dUsers(u1)))^2;
-                        % We assume that alphaChannels include the increase in 
-                        % distance from dUsers(u1) to the real distance traver-
-                        % sed by the signal in on each path
+                              pattern(handle_Conf_Array,problem.freq,...
+                                      problem.phiChannels(u1,i),...
+                                      problem.thetaChannels(u1,i),...
+                                      'Type','Power','Normalize',false)*...
+                                      (problem.lambda/(4*pi*problem.dUsers(u1)))^2;
+                                  
+                    % Consider only antenna directivity (seems more fair)
+                    PRx(u1) = PRx(u1) + problem.alphaChannels(u1,i)*...
+                              patternAzimuth(handle_Conf_Array, problem.freq, ...
+                                             problem.thetaUsers(u), ...
+                                             'Azimuth', problem.phiUsers(u),...
+                                             'Type', 'power');
+                        
                 end
             end
         end
@@ -77,13 +71,31 @@ function [Val] = o_Position_Objective_optim_cost_multipath(Position_Taper, conf,
         Int = sum(PRx)-PRx(problem.IDUserAssigned);
         Int = Int/(numel(PRx)-1);
     end
+    
+    % Consider beamwidth into opt. (efficiency metric)
+    if conf.Fweights(2) > 0
+        % Define variable range
+        azimuth = -180:2:180;
+        elevation = -90:2:90;
+        % Compute bandwidth azymuth
+        bw_az = o_beamwidth(handle_Conf_Array,problem.freq,...
+            azimuth,problem.thetaChannels(problem.IDUserAssigned),3,...
+            conf.PlotAssignments);
+        % Compute bandwidth elevation
+        bw_el = o_beamwidth(handle_Conf_Array,problem.freq,...
+            problem.phiChannels(problem.IDUserAssigned),elevation,3,...
+            conf.PlotAssignments);
+        % Compute final bandwidth
+        HPBW = bw_az * bw_el;
+        if conf.verbosity > 1
+            fprintf('The HPBW (az) computed by the helper function is %f\n',bw_az);
+            fprintf('The HPBW (el) computed by the helper function is %f\n',bw_el);
+        end
+    end
 
     % Computing the function value from the SL_Mag
-    if problem.nUsers > 1
-        Val = conf.Fweights(1)*Int/PR + conf.Fweights(2)*HPBW/(360^2);
-    else
-        Val = conf.Fweights(1)*1/PR + conf.Fweights(2)*HPBW/(360^2);
-    end
+    Val = ( conf.Fweights(1)*(1/PR) * conf.Fweights(2)*(Int) ) + conf.Fweights(3)*(HPBW/(360^2));
+        
     if conf.verbosity > 1
         fprintf('Val = %f\n',Val);
     end
--- o_Position_Objective_optim_cost_singlepath.m
+++ o_Position_Objective_optim_cost_singlepath.m
@@ -1,6 +1,11 @@
 function [Val] = o_Position_Objective_optim_cost_singlepath(Position_Taper, conf, problem)
-% Copyright 2017  The MathWorks, Inc.
-% Define constants
+    
+    % Initialize optimization variables
+    PR   = 0;  % Received power at intended user
+    Int  = 0;  % Interference generated to other users
+    HPBW = 0;  % Beamdiwth achieved
+    
+    % Define constants
     if ~strcmp(conf.genStructure, 'allAntennas')
         nck = strcmp(conf.genStructure, 'nchoosek');
         if sum(Position_Taper(1:problem.Nmax-(problem.Nmax*nck)+1) ~= ...
@@ -11,48 +16,24 @@ function [Val] = o_Position_Objective_optim_cost_singlepath(Position_Taper, conf
         end
     end
 
-    % Setting figures to invisible
-    %set(groot,'defaultFigureVisible','off')
-
-    azimuth = -180:2:180;
-    elevation = -90:2:90;
-
+    % Create antenna subarray
     [Position_Taper_elem,problem] = o_subarrayToAntennaElements(Position_Taper,conf,problem);
     % Extracting taper values from the input vector
     Taper_value = Position_Taper_elem(problem.ant_elem+1:problem.ant_elem*2) .* ...
         exp(1i.*Position_Taper_elem(problem.ant_elem*2+1:problem.ant_elem*3));
-
+    if isempty(Taper_value)
+        fprintf('warning: Taper is empty.\n');
+    end
     % Creating a Conformal Array with cosine elements.
     % Conformal array will be limited to a single plane
-
-    if isempty(Taper_value)
-        disp('uyuyuy');
-    end
-    
     handle_Conf_Array = phased.ConformalArray('Element',problem.handle_Ant,...
         'ElementPosition',[zeros(1,problem.ant_elem);...
         problem.possible_locations(2,Position_Taper_elem(1:problem.ant_elem));...
         problem.possible_locations(3,Position_Taper_elem(1:problem.ant_elem))],...
         'Taper',Taper_value);
     
-    if conf.Fweights(2) > 0
-        bw_az = o_beamwidth(handle_Conf_Array,problem.freq,...
-            azimuth,problem.thetaUsers(problem.IDUserAssigned),3,...
-            conf.PlotAssignments);
-        if conf.verbosity > 1
-            fprintf('The HPBW (az) computed by the helper function is %f\n',bw_az);
-        end
-        bw_el = o_beamwidth(handle_Conf_Array,problem.freq,...
-            problem.phiUsers(problem.IDUserAssigned),elevation,3,...
-            conf.PlotAssignments);
-        if conf.verbosity > 1
-            fprintf('The HPBW (el) computed by the helper function is %f\n',bw_el);
-        end
-
-        HPBW = bw_az * bw_el;
-    end
-    
-    if conf.Fweights(1) > 0
+    % Consider rx power and interference generated into opt. (IUI metric)
+    if conf.Fweights(1)+conf.Fweights(2) > 0
         % Computing the received power in every user
         PRx = zeros(1,problem.nUsers);
         for u=1:problem.nUsers
@@ -63,22 +44,52 @@ function [Val] = o_Position_Objective_optim_cost_singlepath(Position_Taper, conf
 %                     'Type','Power')*...
 %                     (problem.lambda/(4*pi*problem.dUsers(u)))^2;
             % Consider only antenna directivity (seems more fair)
-            PRx(u) = patternAzimuth(handle_Conf_Array, problem.freq, problem.thetaUsers(u), 'Azimuth', problem.phiUsers(u), 'Type', 'power');
+            PRx(u) = patternAzimuth(handle_Conf_Array, problem.freq, ...
+                                    problem.thetaUsers(u), ...
+                                    'Azimuth', problem.phiUsers(u),...
+                                    'Type', 'power');
         end
 
         PR = PRx(problem.IDUserAssigned);
         Int = sum(PRx)-PRx(problem.IDUserAssigned);
         Int = Int/(numel(PRx)-1);
+        
+        if conf.verbosity > 1
+            fprintf('The PR  computed is %.2f\n',10*log10(PR));
+            fprintf('The Int computed is %.2f\n',10*log10(Int));
+        end
     end
+    
+    % Consider beamwidth into opt. (efficiency metric)
+    if conf.Fweights(2) > 0
+        % Define variable range
+        azimuth = -180:2:180;
+        elevation = -90:2:90;
+        % Compute bandwidth azymuth
+        bw_az = o_beamwidth(handle_Conf_Array,problem.freq,...
+            azimuth,problem.thetaUsers(problem.IDUserAssigned),3,...
+            conf.PlotAssignments);
+        % Compute bandwidth elevation
+        bw_el = o_beamwidth(handle_Conf_Array,problem.freq,...
+            problem.phiUsers(problem.IDUserAssigned),elevation,3,...
+            conf.PlotAssignments);
+        % Compute final beamwidth
+        HPBW = bw_az * bw_el;
+        if conf.verbosity > 1
+            fprintf('The HPBW (az) is %f\n',bw_az);
+            fprintf('The HPBW (el) is %f\n',bw_el);
+        end
+    end    
 
     % Computing the function value from the SL_Mag 
-    if problem.nUsers > 1
-%         Val = conf.Fweights(1)*Int/PR + conf.Fweights(2)*HPBW/(360^2);
-        Val = conf.Fweights(1)*Int/PR;
-    else
-        Val = conf.Fweights(1)*1/PR + conf.Fweights(2)*HPBW/(360^2);
-    end
+%     Val = ( conf.Fweights(1)*(1/PR) * conf.Fweights(2)*(Int) ) + conf.Fweights(3)*(HPBW/(360^2));
+    WeightIDmax = (-1)*(1/problem.nUsers);
+    WeightIDmin = (+1)*(1 - 1/problem.nUsers);
+    PRdB  = (10*log10(PR));
+    IntdB = (10*log10(Int));
+    Val = WeightIDmax*PRdB + WeightIDmin*IntdB + conf.Fweights(3)*(HPBW/(360^2));
+    
     if conf.verbosity > 1
-        fprintf('Val = %f\n',Val);
+        fprintf('Val = %f\t1st = %.2f\t2nd = %.2f\t3rd = %.2f\n',Val,PRdB,IntdB,(HPBW/(360^2)));
     end
 end
