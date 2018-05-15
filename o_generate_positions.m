82859e6ccd49a8c34ab429b4c1f1a7741cf79c94
root <bocanegra.c@husky.neu.edu>
2018-05-15 17:38:30 -0500

    Add deterministic positions

--- o_generate_positions.m
+++ o_generate_positions.m
@@ -1,43 +1,54 @@
 function [thetaPos, phiPos, dPos] = o_generate_positions(conf,nUsers,...
-            maxdUsers,mindUsers)
-%% Randomization and conversion of the users' positions in the space
-    %%%% Only in the case where no determinist position is given in the input
-    %%%% file defining the problem. The conversion to degrees is performed if
-    %%%% angles were given in radians
+                                                        maxdUsers,mindUsers)
+    % Generate Normal distribution
     pd = makedist('Normal');
     pd.sigma = 45;
     t = truncate(pd,-45,45);
-    % if ~isfield(problem,'thetaUsers') || isempty(problem.thetaUsers)
-    if conf.verbosity >= 1
-        fprintf('===================================================\n');
-        fprintf('The users were not assigned positions in the space:\n');
-        fprintf('Assigning random values...\n');
-    end
-    thetaPos = random(t,1,nUsers);
-    if conf.verbosity >= 1
-        fprintf('New elevations assigned:\n');
-        display(thetaPos);
-    end
-%     elseif conf.anglesInRadians
-%         thetaPos = problem.thetaUsers/(2*pi)*360;
-%     else % Nothing to do
-%         thetaPos = problem.thetaUsers;
-%     end
-    % if ~isfield(problem,'phiUsers') || isempty(problem.phiUsers)
-    phiPos = random(t,1,nUsers);
-    if conf.verbosity >= 1
-        fprintf('New azimuths assigned:\n');
-        display(phiPos);
-    end
-%     elseif conf.anglesInRadians
-%         phiPos = problem.phiUsers/(2*pi)*360;
-%     else % Nothing to do
-%         phiPos = problem.phiUsers;
-%     end
-    % if ~isfield(problem,'phiUsers') || isempty(problem.phiUsers)
-    dPos = rand(1,10) * (maxdUsers-mindUsers) + mindUsers;
-%     else % Nothing to do
-%         dPos = problem.dUsers;
-%     end
-end
 
+    % Check if users were assigned Theta angles (deterministic)
+    if ~conf.detLocation || ~isfield(conf,'thetaUsers')
+        % Generate Elevation angles
+        thetaPos = random(t,1,nUsers);
+        if conf.verbosity >= 1
+            fprintf('Users were not assigned Theta values (positions) in the space.\n');
+        end
+    elseif conf.anglesInRadians
+        % Convert Theta from radians to degrees
+        thetaPos = conf.thetaUsers(1:nUsers)/(2*pi)*360;
+    else
+        % Deterministic Theta angles in degrees
+        thetaPos = conf.thetaUsers(1:nUsers);
+    end
+
+    % Check if users were assigned Phi angles (deterministic)
+    if ~conf.detLocation || ~isfield(conf,'phiUsers')
+        % Generate Azymuth angles
+        phiPos = random(t,1,nUsers);
+        if conf.verbosity >= 1
+            fprintf('Users were not assigned Theta values (positions) in the space.\n');
+        end
+    elseif conf.anglesInRadians
+        % Convert Phi from radians to degrees
+        phiPos = conf.phiUsers(1:nUsers)/(2*pi)*360;
+    else
+        % Deterministic Phi angles in degrees
+        phiPos = conf.phiUsers(1:nUsers);
+    end
+
+    % Check if users were assigned Distances (deterministic)
+    if ~conf.detLocation || ~isfield(conf,'dUsers')
+        dPos = rand(1,nUsers) * (maxdUsers-mindUsers) + mindUsers;
+    else
+        dPos = conf.dUsers(1:nUsers);
+    end
+
+    % Display user distribution in space
+    if conf.verbosity >= 1
+        fprintf('Elevations assigned:\n');
+        display(thetaPos);
+        fprintf('Azimuths assigned:\n');
+        display(phiPos);
+        fprintf('Distances to BS assigned:\n');
+        display(dPos);
+    end 
+end
