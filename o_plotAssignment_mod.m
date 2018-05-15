34436493a78559ec5a1e8999132c967281c4bd82
root <bocanegra.c@husky.neu.edu>
2018-05-15 17:41:46 -0500

    Fixed small bugs on visualization function

--- o_plotAssignment_mod.m
+++ o_plotAssignment_mod.m
@@ -4,37 +4,33 @@ function o_plotAssignment_mod(problem, handle_Conf_Array)
     IDmax = problem.IDUserAssigned;  % Desired user
     IDmin = 1:1:problem.nUsers;
     IDmin(IDmin==problem.IDUserAssigned) = [];  % Users interfereed
+    azimuth = -180:1:180;  % Azymuth angle range
+    elevation = -90:1:90;  % Elevation angle range
+    
     % Array geometry and pattern for optimized values.
     figure;
     handle_Conf_Array.viewArray;
     figure;
     handle_Conf_Array.pattern(problem.freq,'Type','powerdb');
-    %%
     hold on
-    dx = 0.1;
-    dy = 0.1;
-    dz = 0.3;
+    dx = 0.1;   dy = 0.1;   dz = 0.3;
     [x_u,y_u,z_u] = sph2cart(problem.phiUsers/360*2*pi,...
         problem.thetaUsers/360*2*pi,...
         3+50*ones(size(problem.phiUsers)));%3*10^(max(max(p))/10)*
+    % Visualize user location/channels
     for i = 1:problem.nUsers
         line([0,x_u(i)],[0,y_u(i)],[0,z_u(i)]);
-        if i == IDmax
-            scatter3(x_u(i),y_u(i),z_u(i),'r');
-        else
-            scatter3(x_u(i),y_u(i),z_u(i),'g');
+        if i == IDmax;   scatter3(x_u(i),y_u(i),z_u(i),'r');
+        else;            scatter3(x_u(i),y_u(i),z_u(i),'g');
         end
         text(x_u(i)+dx, y_u(i)+dy, z_u(i)+dz, ['User ',num2str(i)]);
     end
-    %%
-    azimuth = -180:1:180;
-    elevation = -90:1:90;
     
-    fprintf('Plotting selected pattern for user %d\n',IDmax);
-    fprintf('* %d [Theta: %.1f - Phi: %.1f]\n',IDmax,problem.thetaUsers(IDmax),problem.phiUsers(IDmax));
-    for idx = 1:length(IDmin)
-        fprintf('  %d [Theta: %.1f - Phi: %.1f]\n',IDmin(idx),problem.thetaUsers(IDmin(idx)),problem.phiUsers(IDmin(idx)));
-    end
+%     fprintf('Plotting selected pattern for user %d\n',IDmax);
+%     fprintf('* %d [Theta: %.1f - Phi: %.1f]\n',IDmax,problem.thetaUsers(IDmax),problem.phiUsers(IDmax));
+%     for idx = 1:length(IDmin)
+%         fprintf('  %d [Theta: %.1f - Phi: %.1f]\n',IDmin(idx),problem.thetaUsers(IDmin(idx)),problem.phiUsers(IDmin(idx)));
+%     end
     
     figure;
     % Plot Theta cuts - specify Phi
@@ -42,7 +38,7 @@ function o_plotAssignment_mod(problem, handle_Conf_Array)
     subplot(2,problem.nUsers,1);
     tt = strcat('User',{' '},num2str(IDmax),{' '},'Phi:',{' '},num2str(problem.phiUsers(IDmax)));
     pattAz = patternAzimuth(handle_Conf_Array,problem.freq,problem.thetaUsers(IDmax),'Azimuth',azimuth,'Type','powerdb'); 
-    polarpattern(azimuth,pattAz,'MagnitudeLim',[-10 max(pattAz)],'AntennaMetrics',0,'TitleTop',tt{:},'TitleTopFontSizeMultiplier',2);
+    polarpattern(azimuth,pattAz,'MagnitudeLim',max(pattAz)+[-50 0],'AntennaMetrics',0,'TitleTop',tt{:},'TitleTopFontSizeMultiplier',2);
     hold on;
     mark = personalMarker(azimuth,pattAz,problem.phiUsers(IDmax));
     polarpattern(azimuth,mark);
@@ -63,7 +59,7 @@ function o_plotAssignment_mod(problem, handle_Conf_Array)
     subplot(2,problem.nUsers,problem.nUsers+1)
     tt = strcat('User',{' '},num2str(IDmax),{' '},'Theta:',{' '},num2str(problem.thetaUsers(IDmax)));
     pattEl = patternElevation(handle_Conf_Array,problem.freq,problem.phiUsers(IDmax),'Elevation',elevation,'Type','powerdb');
-    polarpattern(elevation,pattEl,'MagnitudeLim',[-10 max(pattEl)],'AntennaMetrics',0,'TitleTop',tt{:},'TitleTopFontSizeMultiplier',2);
+    polarpattern(elevation,pattEl,'MagnitudeLim',max(pattAz)+[-50 0],'AntennaMetrics',0,'TitleTop',tt{:},'TitleTopFontSizeMultiplier',2);
     hold on;
     mark = personalMarker(elevation,pattEl,problem.thetaUsers(IDmax));
     polarpattern(elevation,mark);
@@ -79,7 +75,7 @@ function o_plotAssignment_mod(problem, handle_Conf_Array)
         polarpattern(elevation,mark);
     end
     
-    suptt = strcat('Radiation pattern for user',{' '},num2str(IDmax));
+    suptt = strcat('Pattern for user',{' '},num2str(IDmax),{' '},'(Nant =',{' '},num2str(problem.N_Antennas));
     tit = suptitle(suptt{:});
     set(tit,'FontSize',14)
 end
